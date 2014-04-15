0e6df091730dff0f086fcbe952a97714e1f0b9c4
Ryan Regalado <ryan@design48.net>
2014-04-15 09:36:10 -0700

    new pages

---
+++ _site/assets/themes/tom/js/app.js
@@ -0,0 +1,43 @@
+document.onready = function() {
+  (function(){
+
+    /**
+     * Creates all images with ability to click to view larger image in
+     * separate window.
+     * 
+     * @return void Creates event listeners
+     */
+    function setImageLinks() {
+      var parent = document.getElementById('post')
+        , images = parent.getElementsByTagName('img')
+        ;
+
+      for(var i = 0, len = images.length; i < len; i++){
+        // bind i variable to a function not inside the same scope
+        (function(index) {
+          var src = images[index].getAttribute('src');
+
+          images[index].addEventListener('click', function setImageClickEvent() {
+              window.open(src);
+          }, false);
+
+        }(i));
+      }
+    }
+
+
+    /**
+     * Kick it off essay
+     * 
+     * @return void Start the life cycle
+     */
+    function init() {
+      setImageLinks();
+    }
+
+    /**
+     * Kick this piece off yo
+     */
+    init();
+  }());
+}
